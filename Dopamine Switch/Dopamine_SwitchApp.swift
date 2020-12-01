e54ba9d0f07f28d6a2ec12fd079ba09723988632
Oliver <oliver.lingertat@gmail.com>
2020-12-01 15:57:33 -0500

    Initial Commit

---
+++ Dopamine Switch/Dopamine_SwitchApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dopamine_SwitchApp.swift
+//  Dopamine Switch
+//
+//  Created by Oliver's probook on 12/1/20.
+//
+
+import SwiftUI
+
+@main
+struct Dopamine_SwitchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
