7b45303931bb1d475827591891fe6e3fdca984c3
Oliver <oliver.lingertat@gmail.com>
2020-12-01 22:22:16 -0500

    Menu bar icon that opens and closes NSPopover object

--- Dopamine Switch/Dopamine_SwitchApp.swift
+++ Dopamine Switch/Dopamine_SwitchApp.swift
@@ -9,9 +9,47 @@ import SwiftUI
 
 @main
 struct Dopamine_SwitchApp: App {
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     var body: some Scene {
-        WindowGroup {
-            ContentView()
+        Settings {
+            EmptyView()
+        }
+    }
+}
+
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+    var popover = NSPopover.init()
+    var statusBarItem: NSStatusItem?
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        
+        let contentView = ContentView()
+
+        // Set the SwiftUI's ContentView to the Popover's ContentViewController
+        popover.behavior = .transient
+        popover.animates = false
+        popover.contentViewController = NSViewController()
+        popover.contentViewController?.view = NSHostingView(rootView: contentView)
+        statusBarItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.squareLength)
+        statusBarItem?.button?.image = NSImage(named:NSImage.Name("StatusBarButtonImage"))
+        statusBarItem?.button?.action = #selector(AppDelegate.togglePopover(_:))
+    }
+    @objc func showPopover(_ sender: AnyObject?) {
+        if let button = statusBarItem?.button {
+            popover.show(relativeTo: button.bounds, of: button, preferredEdge: .minY)
+            popover.contentViewController?.view.window?.makeKey() // this is required for popover.behavior=.transient to have effect
+            // !!! There is a problem where closing the popover by clicking the menu bar, then clicking again without moving the mouse fails to open the popover
+        }
+    }
+    @objc func closePopover(_ sender: AnyObject?) {
+        popover.performClose(sender)
+    }
+    @objc func togglePopover(_ sender: AnyObject?) {
+        if popover.isShown {
+            closePopover(sender)
+        } else {
+            showPopover(sender)
         }
     }
 }
